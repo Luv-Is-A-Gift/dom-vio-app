4da4a86a2d5b077242a8b529b7e8a8b934b8f661
chadwick <cekeller6121@yahoo.com>
2017-08-28 16:44:26 -0400

    add files, start project

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const mustacheExpress = require('mustache-express');
+const session = require('express-session');
+const path = require('path');
+const bodyParser = require('body-parser');
+const publicPath = path.resolve(__dirname, 'public');
+
+var app = express();
+
+app.engine('mustache', mustacheExpress());
+app.set('views', './views');
+app.set('view engine', 'mustache');
+app.use(express.static(publicPath));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(session({ secret: 'dom vio', cookie: { maxAge: 300000 }}));
+
+app.get('/', function(req, res) {
+  req.session.authenticated = false;
+  res.render('home');
+});
+
+app.get('/admin', function(req, res) {
+  if (req.session.authenticated === true) {
+    res.render('admin')
+  } else {
+    res.redirect('/');
+  }
+});
+
+
+
+
+
+
+
+
+
+
+app.listen(process.env.PORT || 5000, function(req, res) {
+  console.log("dom vio app up on port 5000");
+});
+
+
+
+
+
+// *** intentional whitespace ***
