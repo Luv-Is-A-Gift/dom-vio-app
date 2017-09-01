8599e3709af708e01afe522be9b1af7e1a8043e4
Geoff Humphreys <humphreys.geoff@gmail.com>
2017-09-01 15:06:11 -0400

    update UserSchema

--- models/User.js
+++ models/User.js
@@ -6,22 +6,25 @@ const userSchema = mongoose.Schema({
     lastname: {type: String, required: true},
     username: {type: String, required: true, unique: true},
     password: {type: String, required: true},
-    email: {type: String, required: true, unique: true},
+    email: {type: String, unique: true},
     phone_number: {type: String, required: true, unique: true},
-    address: {type: String, required: true},
+    homeAddress: {type: String, required: true},
     addressInfo: {type: String},
-    safety_contacts:[{
-      firstname: {type: String, required: true, unique: true},
-      lastname: {type: String, required: true, unique: true},
-      relationshipToUser: {type: String, require: true},
-      email: {type: String, required: true, unique: true},
-      phone_number: {type: String, required: true, unique: true},
+    addlAddress: {type: String, required: true},
+
+    safety_contact
+    :[{
+      firstname: {type: String, unique: true},
+      lastname: {type: String, unique: true},
+      relationshipToUser: {type: String},
+      email: {type: String},
+      phone_number: {type: String},
     }],
     logs:[{
       timestamp: {type: Date, default: Date.now},
-      location: {type: String, required: true},
-      details: {type: String, required: true, unique: true, maxlength: 2000},
-      level_of_situation: {type: Number, required: true}
+      location: {type: String},
+      details: {type: String, maxlength: 2000},
+      level_of_situation: {type: Number}
     }]
 
 });
