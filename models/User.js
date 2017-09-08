bed8b16d9dd752327cd63c9fbb2548a2cf93b9b9
Geoff Humphreys <humphreys.geoff@gmail.com>
2017-09-08 15:57:24 -0400

    add contact safe space 911 and back to game on user-home

--- models/User.js
+++ models/User.js
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-const mongoose = require('mongoose')
-
-
-const userSchema = mongoose.Schema({
-    firstname: {type: String, required: true},
-    lastname: {type: String, required: true},
-    username: {type: String, required: true, unique: true},
-    password: {type: String, required: true},
-    email: {type: String, unique: true},
-    phone_number: {type: String, required: true, unique: true},
-    homeAddress: {type: String, required: true},
-    addressInfo: {type: String},
-    addlAddress: {type: String, required: true},
-
-    safety_contact
-    :[{
-      firstname: {type: String, unique: true},
-      lastname: {type: String, unique: true},
-=======
 const mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 const bcrypt = require('bcryptjs');
@@ -38,7 +18,6 @@ const userSchema = new mongoose.Schema({
     :[{
       firstname: {type: String},
       lastname: {type: String},
->>>>>>> c63fdd36de8329e46f35934f78522d74633e21c8
       relationshipToUser: {type: String},
       email: {type: String},
       phone_number: {type: String},
@@ -47,19 +26,11 @@ const userSchema = new mongoose.Schema({
       timestamp: {type: Date, default: Date.now},
       location: {type: String},
       details: {type: String, maxlength: 2000},
-<<<<<<< HEAD
-      level_of_situation: {type: Number}
-=======
       level_of_situation: {type: String}
->>>>>>> c63fdd36de8329e46f35934f78522d74633e21c8
     }]
 
 });
 
-<<<<<<< HEAD
-const User = mongoose.model('User', userSchema)
-module.exports = User
-=======
 userSchema.pre('save', function (next) {
    if (!this.isModified('password')) {
       return next();
@@ -72,4 +43,3 @@ userSchema.pre('save', function (next) {
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
->>>>>>> c63fdd36de8329e46f35934f78522d74633e21c8
